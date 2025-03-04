d71d6d1e2cf3ed20cc92801aa28b44e8ce974558
AmmarAhmed <ammarahmed1263@gmail.com>
2025-03-04 23:25:51 +0200

    separate utilities

---
+++ utils/getThisMonthLogs.tsx
@@ -0,0 +1,26 @@
+import { BodyConditionLog, WeightLog } from "@/types";
+
+function getThisMonthLogs(logs_bodycondition: BodyConditionLog[], logs_weight: WeightLog[]) {
+  const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
+
+  const latestBodyConditionLog = logs_bodycondition
+    .filter(
+      (log) =>
+        new Date(log.date).getMonth() === currentMonth &&
+        new Date(log.date).getFullYear() === currentYear
+    )
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())[0];
+
+  const latestWeightLog = logs_weight
+    .filter(
+      (log) =>
+        new Date(log.date).getMonth() === currentMonth &&
+        new Date(log.date).getFullYear() === currentYear
+    )
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())[0];
+
+  return { latestBodyConditionLog, latestWeightLog };
+}
+
+export default getThisMonthLogs;
---
+++ utils/index.ts
@@ -0,0 +1,1 @@
+export {default as getThisMonthLogs} from './getThisMonthLogs';
