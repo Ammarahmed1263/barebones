3ff793e0c66a001fd6ba89578c731e23d4b8c71e
AmmarAhmed <ammarahmed1263@gmail.com>
2025-03-04 23:24:33 +0200

    create tab navigator

---
+++ components/TabNavigator.tsx
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { FC } from 'react'
+import AppButton from './AppButton'
+import { LogType } from '@/types'
+
+interface TabNavigatorProps {
+  activeTab: LogType;
+  setActiveTab: React.Dispatch<React.SetStateAction<LogType>>
+}
+
+const TabNavigator: FC<TabNavigatorProps> = ({activeTab, setActiveTab}) => {
+  const tabs = [
+    { id: 'weight', title: 'Weight' },
+    { id: 'body', title: 'Body Condition' },
+    { id: 'vet', title: 'Vet Visits' },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {tabs.map(tab => (
+        <AppButton 
+          style={[styles.tabItem, activeTab === tab.id && styles.activeTabItem]}
+          titleStyle={[styles.title, activeTab === tab.id && styles.activeTitle]}
+          title={tab.title}
+          onPress={() => setActiveTab(tab.id)} />
+      ))}
+    </View>
+  )
+}
+
+export default TabNavigator
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    width: '100%',
+    marginVertical: 20,
+    borderTopWidth: 3,
+    borderColor: '#1E90FF'
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 8,
+  },
+  activeTabItem: {
+    borderBottomWidth: 2,
+    borderColor: '#1E90FF'
+  },
+  title: {
+    fontSize: 14,
+  },
+  activeTitle: {
+    color: '#1E90FF',
+    fontWeight: '700'
+  }
+})
